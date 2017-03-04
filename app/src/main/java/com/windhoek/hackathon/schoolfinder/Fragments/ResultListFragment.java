b3bd48143d2e353fff629c1c8354d44f362b384b
Matti <matti.maki-kihnia@metropolia.fi>
2017-03-04 19:51:44 +0200

    Started to work ont he list view

--- app/src/main/java/com/windhoek/hackathon/schoolfinder/Fragments/ResultListFragment.java
+++ app/src/main/java/com/windhoek/hackathon/schoolfinder/Fragments/ResultListFragment.java
@@ -1,26 +1,55 @@
 package com.windhoek.hackathon.schoolfinder.Fragments;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.windhoek.hackathon.schoolfinder.MainActivity;
 import com.windhoek.hackathon.schoolfinder.R;
 
 /**
  * Created by Asus on 04/03/2017.
  */
 
-public class ResultListFragment extends Fragment {
+public class ResultListFragment extends Fragment implements AdapterView.OnItemClickListener {
     private static final String TAG = "ResultListFragment";
     private View fragmentView;
+    private ListView resultListView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         this.fragmentView = inflater.inflate(R.layout.fragment_result_list, container, false);
+
+        this.resultListView = (ListView) this.fragmentView.findViewById(R.id.resultListView);
+
+        setupListView();
         return this.fragmentView;
     }
+
+    private void setupListView() {
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getMainActivity(), R.layout.fragment_result_list_item);
+        this.resultListView.setAdapter(arrayAdapter);
+        this.resultListView.setOnItemClickListener(this);
+    }
+
+    private MainActivity getMainActivity() {
+        return (MainActivity) getActivity();
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Log.e(TAG, "onItemClick: item was clicked" );
+    }
+
+
 }
